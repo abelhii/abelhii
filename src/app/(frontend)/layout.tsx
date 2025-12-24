00c51babf28eecee6f8126b0c346138fc9ddb386
Abel <9299622+abelhii@users.noreply.github.com>
2025-12-24 00:44:31 +0000

    add vercel analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import './globals.css'
+import '../globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   description: 'A Portfolio website using Payload in a Next.js app.',
@@ -11,6 +12,9 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        <Analytics />
+      </head>
       <body>
         <main>{children}</main>
       </body>
